f23f9de192d2199b276fa166e6c3808ee2b15bb6
Konstantin Semenov <kvsemenoff@mail.ru>
2016-05-28 12:17:21 +0400

    dd

---
+++ includes/header_bottom.php
@@ -0,0 +1,9 @@
+<section class="aa-header-bottom">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<img src="img/main_tel.png" alt="">
+			</div>
+		</div>
+	</div>
+</section>
