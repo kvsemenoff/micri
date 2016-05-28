b67a8323c20c2e00bb4feacebefac36b165a7dd8
Konstantin Semenov <kvsemenoff@mail.ru>
2016-05-28 14:25:45 +0400

    стрниаца htlp и помощь

--- index.php
+++ index.php
@@ -9,7 +9,27 @@
 	<?php require_once('includes/header_top.php'); ?>
 	<?php require_once('includes/header_middle.php'); ?>
 	<?php require_once('includes/header_bottom.php'); ?>
-	<?php require_once('includes/main_page.php'); ?>
+
+
+	<div class="container simple-page">
+		<div class="row">
+			<div class="col-md-6">
+				<h2>Как это работает</h2>
+				<p>
+					<strong>MICRI</strong> - это кросс-платформенное приложение для смартфонов, позволяющее обмениваться сообщениями и при этом не платить, как за SMS. Приложение доступно для Android,  и, конечно, сообщения могут передаваться между всеми указанными смартфонами. использует тот же тарифный план Интернета, что и электронная почта с браузером, поэтому при передаче сообщений отдельная плата взиматься не будет, и вы всегда сможете оставаться на связи с друзьями.
+				</p>
+				<p>
+				Кроме стандартного обмена сообщениями пользователи <strong>MICRI</strong> могут создавать группы, отправлять друг другу неограниченное число сообщений, фотографий, аудио- и видеофайлов. 
+
+				</p>
+			</div>
+			<div class="col-md-6">
+				<img src="img/tel2.png" alt="" class="tel2">
+			</div>
+		</div>
+	</div>
+
+
 	<?php require_once('includes/footer.php'); ?>
 	
 
