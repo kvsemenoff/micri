5f2d5669aba41ee32e730c9570817039326ebb14
anzarsh <anzarsh@mail.ru>
2016-05-28 16:56:54 +0300

    верхнее меню-2

--- includes/header_top.php
+++ includes/header_top.php
@@ -1,3 +1,5 @@
+<div id="aa-dark-body" class=""></div>
+
 <section class="header-top">
 		<div class="container">
 			<div class="row">
@@ -8,25 +10,26 @@
 					</div>
 				</div>
 
-				<div class="col-md-3 hidden-xs">
-					<span class="aa-degree">Конфиденциальность на высшем уровне</span>
+				<div class="col-md-4 col-sm-6 hidden-xs">
+
+					<span class="aa-degree">Конфиденциальность<br>на высшем уровне</span>
 				</div>
 				
-				<div class="col-md-3 hidden-xs">
+				<div class="col-md-2 hidden-sm hidden-xs">
 					<a href="" class="aa-key">
-						Купить ключ
+						Купить ключь
 					</a>
 				</div>
 				
-				<div class="col-md-3 hidden-xs">
+				<div class="col-md-3 hidden-sm hidden-xs">
 					<a href="" class="aa-question">
 						Задать вопрос
 					</a>
 				</div>
 				
-				<div class="hidden-lg hidden-md col-sm-6 col-xs-6">
+				<div class="hidden-lg hidden-md hidden-sm col-xs-6">
 						<span class="aa-menu">
-							<span id="icon2" class=""><i class="fa fa-bars" aria-hidden="true"></i>
+							<span><i class="fa fa-bars" aria-hidden="true"></i>
 							</span>
 						</span>
 				</div>
@@ -35,16 +38,16 @@
 			</div>
 		</div>
 </section>
-<section class="header-middle">
+<section class="header-middle header-middle-2">
 		<div class="container">
 			<div class="row">
 				<ul class="menu-list-unpressed">
-					<li class="aa-menu-item"><a href="#" class="">Как это работает</a></li>
-					<li class="aa-menu-item"><a href="#" class="">Оплата</a></li>
-					<li class="aa-menu-item"><a href="#" class="">Документы</a></li>
-					<li class="aa-menu-item"><a href="#" class="">Загрузка</a></li>
-					<li class="aa-menu-item"><a href="#" class="">Помощь</a></li>
-					<li class="aa-menu-item"><a href="#" class="">Контакты</a></li>
+					<li class="aa-menu-item aa-menu-item-2"><a href="#" class="">Как это работает</a></li>
+					<li class="aa-menu-item aa-menu-item-2"><a href="#" class="">Оплата</a></li>
+					<li class="aa-menu-item aa-menu-item-2"><a href="#" class="">Документы</a></li>
+					<li class="aa-menu-item aa-menu-item-2"><a href="#" class="">Загрузка</a></li>
+					<li class="aa-menu-item aa-menu-item-2"><a href="#" class="">Помощь</a></li>
+					<li class="aa-menu-item aa-menu-item-2"><a href="#" class="">Контакты</a></li>
 				</ul>
 			</div>
 		</div>
@@ -55,7 +58,13 @@
 		//alert(1);
 		$('.aa-menu').click(function(){
 			$('.menu-list-unpressed').toggleClass('header-top__menu-pressed');
-		})
+			$('#aa-dark-body').toggleClass('aa-dark-body');
+		});
+		$('#aa-dark-body').click(function(){
+			$('.menu-list-unpressed').toggleClass('header-top__menu-pressed');
+			$(this).toggleClass('aa-dark-body');
+		});
+
 	});
 
 </script>
